371cbd2e11211a612376f70c1a5ea28e84696f6d
SarthakJain01 <88705836+SarthakJain01@users.noreply.github.com>
2022-03-21 23:52:46 +1030

    Add files via upload

--- Human.h
+++ Human.h
@@ -1,14 +1,15 @@
 #ifndef HUMAN_H
 #define HUMAN_H
+
 #include <string>
 #include <iostream>
 #include "Player.h"
-#include "Human.h"
 using namespace std;
 
-Human::Human() {}
-
-void Human::play() {
-  getline(cin, input);
+class Human:public Player{
+    public:
+        Human();
+        void play();
 };
+
 #endif
