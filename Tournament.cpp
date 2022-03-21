96a6da96cfc3dea39166273d7dde75ac2857269b
SarthakJain01 <88705836+SarthakJain01@users.noreply.github.com>
2022-03-21 23:40:14 +1030

    Add files via upload

--- Tournament.cpp
+++ Tournament.cpp
@@ -1,11 +1,16 @@
 #include <string>
 #include <iostream>
-#include <sstream>
 #include "Player.h"
 #include "Computer.h"
 #include "Human.h"
 #include "Referee.h"
-#include "Type.h"
+#include "Avalanche.h"
+#include "Bureaucrat.h"
+#include "Crescendo.h"
+#include "Fistfull0Dollars.h"
+#include "PaperDoll.h"
+#include "RandomComputer.h"
+#include "Toolbox.h"
 #include "Tournament.h"
 using namespace std;
 
